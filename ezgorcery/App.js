a3fbeb0c87aaa5459d7921170d04aea578695667
Feng Sao <wildsaofeng@gmail.com>
2019-10-25 14:56:58 -0700

    upload templates

--- ezgorcery/App.js
+++ ezgorcery/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Authenticate: AuthenticateScreen,
+    UserType: UserTypeScreen,
+    UserProfile: UserProfileScreen,
+    UserPreference: UserPreferencesScreen,
+    UserVerification: UserVerificationScreen,
   },
-});
+  {
+    initialRouteName: 'Login',
+    defaultNavigationOptions: {
+      title: 'EzGorcery'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
