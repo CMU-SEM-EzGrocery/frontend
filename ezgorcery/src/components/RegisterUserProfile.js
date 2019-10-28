d00237827734e9115bf15578c84beb872135e91c
Feng Sao <wildsaofeng@gmail.com>
2019-10-28 16:29:01 -0700

    Survival MileStone 2

--- ezgorcery/src/components/RegisterUserProfile.js
+++ ezgorcery/src/components/RegisterUserProfile.js
@@ -3,59 +3,58 @@ import { Text, StyleSheet, View, Button, TouchableOpacity, TextInput } from 'rea
 
 const RegisterUserProfile = (props) => {
   return (
+
     <View>
-      <Text style={styles.text}>Tell us something about yourself</Text>
-
-      <Text>First Name</Text>
+      <Text style={styles.headline}>Tell us something about yourself</Text>
+      <Text style={styles.label}>First Name</Text>
       <TextInput style={styles.input}
         autoCapitalize="none"
         onSubmitEditing={() => this.passwordInput.focus()}
         autoCorrect={false}
+        onChangeText= {newValue => props.changeFirstName(newValue) } 
         keyboardType=''
         returnKeyType="next"
-        onChangeText={newText => props.changeFirstName(newText)}
-        placeholder='First Name'
+        placeholder='Enter First Name'
         placeholderTextColor='rgba(225,225,225,0.7)'></TextInput>
-
-      <Text>Last Name</Text>
+      <Text style={styles.label}>Last Name</Text>
       <TextInput style={styles.input}
         autoCapitalize="none"
         onSubmitEditing={() => this.passwordInput.focus()}
         autoCorrect={false}
         keyboardType=''
         returnKeyType="next"
-        onChangeText={newText => props.changeLastName(newText)}
-        placeholder='Last Name'
+        onChangeText= {newValue => props.changeLastName(newValue) } 
+        placeholder='Enter Last Name'
         placeholderTextColor='rgba(225,225,225,0.7)'></TextInput>
-
-      <Text>Phone Number</Text>
+      <Text style={styles.label}>Phone</Text>
       <TextInput style={styles.input}
         autoCapitalize="none"
         onSubmitEditing={() => this.passwordInput.focus()}
         autoCorrect={false}
         keyboardType=''
         returnKeyType="next"
-        onChangeText={newText => props.changePhone(newText)}
-        placeholder='(xxx) xxx - xxxx'
+        onChangeText= {newValue => props.changePhone(newValue) } 
+        placeholder='+1 XXX - XXX - XXX'
         placeholderTextColor='rgba(225,225,225,0.7)'></TextInput>
-
-      <Text>Password</Text>
+      <Text style={styles.label}>Password</Text>
       <TextInput style={styles.input}
         autoCapitalize="none"
         onSubmitEditing={() => this.passwordInput.focus()}
         autoCorrect={false}
         keyboardType=''
-        returnKeyType="next"
-        onChangeText={newText => props.changePassword(newText)}
-        placeholder='Password'
         secureTextEntry={true}
+        onChangeText= {newValue => props.changePassword(newValue) } 
+        returnKeyType="next"
+        placeholder='Enter Password'
         placeholderTextColor='rgba(225,225,225,0.7)'></TextInput>
-        
+
       <Button
+        style={styles.buttonS}
         onPress={() => props.toUserType()}
         title="Go Back"
       ></Button>
       <Button
+        style={styles.buttonS}
         onPress={() => props.toUserPreference()}
         title="Next"
       ></Button>
@@ -66,7 +65,69 @@ const RegisterUserProfile = (props) => {
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
-  }
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 100,
+  },
+  input: {
+    textAlign: 'left',
+    backgroundColor: 'grey',
+    borderColor: 'white',
+    borderWidth: 2,
+    borderRadius: 12,
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
+    overflow: 'hidden',
+    padding: 10,
+    width: "95%",
+  },
+  headline: {
+    fontSize: 20,
+    textAlign: "center",
+    padding: 10,
+    margin: 10,
+    fontWeight: 'bold',
+  },
+  labelBtn: {
+    fontSize: 20,
+    textAlign: "left",
+    marginLeft: 5,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  label: {
+    fontSize: 20,
+    textAlign: "left",
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  labelSmall: {
+    fontSize: 20,
+    textAlign: "left",
+    marginLeft: 110,
+    fontWeight: 'bold',
+  },
+  buttonS: {
+    backgroundColor: 'green',
+    borderColor: 'white',
+    borderWidth: 2,
+    borderRadius: 12,
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
+    overflow: 'hidden',
+    paddingLeft: 80,
+    paddingTop: 20,
+    paddingBottom: 20,
+    textAlign: 'center',
+    color: '#fff',
+    width: "50%",
+    marginLeft: 100,
+    marginTop: 20,
+  },
 });
 
 export default RegisterUserProfile;
--- ezgorcery/src/components/RegisterUserType.js
+++ ezgorcery/src/components/RegisterUserType.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const onRequesterSubmit = () => {
-
-}
 
 const RegisterUserType = (props) => {
   return (
@@ -17,7 +14,7 @@ const RegisterUserType = (props) => {
           props.toUserProfile()
         }
         } 
-        style={styles.buttonS}>
+        style={styles.buttonR}>
         <Text style={styles.labelBtn1}>Requester</Text>
       </TouchableOpacity>
       
@@ -27,7 +24,7 @@ const RegisterUserType = (props) => {
           props.toUserProfile()
         }
         } 
-        style={styles.buttonS}>
+        style={styles.buttonH}>
         <Text style={styles.labelBtn}>Helper</Text>
       </TouchableOpacity>
       
@@ -47,59 +44,77 @@ const styles = StyleSheet.create({
   // container: {
   //   flex: 1,
   //   justifyContent: 'center',
-  //   marginBottom: 200,
   // },
   headline: {
     fontSize: 50,
-    textAlign: "center",
-    padding: 10,
+    textAlign:"center",
+    padding: 10, 
     margin: 10,
-    fontWeight: 'bold',
-  },
-  labelBtn1: {
-    fontSize: 20,
-    textAlign: "left",
-    fontWeight: 'bold',
-    color: 'white',
-    marginRight: 35,
-  },
-  labelBtn: {
-    fontSize: 20,
-    textAlign: "left",
-    fontWeight: 'bold',
-    color: 'white',
-    marginRight: 20,
-  },
+    fontWeight:'bold',      
+},
+labelBtn1: {
+  fontSize: 20,
+  textAlign:"left",
+  fontWeight:'bold',
+  color:'white',
+  marginRight:-40,
+},
+labelBtn: {
+  fontSize: 20,
+  textAlign:"left",
+  fontWeight:'bold',
+  color:'white',
+  marginRight:20,
+},
   label: {
     fontSize: 20,
-    textAlign: "left",
-    marginLeft: 100,
-    fontWeight: 'bold',
+    textAlign:"left",
+    marginLeft: 100, 
+    fontWeight:'bold',  
   },
   labelSmall: {
     fontSize: 20,
-    textAlign: "left",
-    marginLeft: 110,
-    fontWeight: 'bold',
+    textAlign:"left",
+    marginLeft: 110, 
+    fontWeight:'bold',  
   },
-  buttonS: {
-    backgroundColor: 'green',
-    borderColor: 'white',
-    borderWidth: 2,
-    borderRadius: 12,
-    color: '#fff',
-    fontSize: 14,
-    fontWeight: 'bold',
-    overflow: 'hidden',
-    paddingLeft: 80,
-    paddingTop: 20,
-    paddingBottom: 20,
-    textAlign: 'center',
-    color: '#fff',
-    width: "50%",
-    marginLeft: 100,
-    marginTop: 20,
-  },
+buttonR: {
+  backgroundColor: 'green',
+  borderColor: 'white',
+  borderWidth: 2,
+  borderRadius: 12,
+  color: '#fff',
+  fontSize: 14,
+  fontWeight: 'bold',
+  overflow: 'hidden',
+  paddingLeft:58,
+  paddingTop:20,
+  paddingBottom:20,
+  textAlign:'center',
+  color:'#fff',
+  width:"50%",
+  marginLeft:100,
+  marginTop:20,    
+},
+buttonH: {
+  backgroundColor: 'green',
+  borderColor: 'white',
+  borderWidth: 2,
+  borderRadius: 12,
+  color: '#fff',
+  fontSize: 14,
+  fontWeight: 'bold',
+  overflow: 'hidden',
+  paddingLeft:68,
+  paddingTop:20,
+  paddingBottom:20,
+  textAlign:'center',
+  color:'#fff',
+  width:"50%",
+  marginLeft:100,
+  marginTop:20,
+  
+},
 });
 
 export default RegisterUserType;
